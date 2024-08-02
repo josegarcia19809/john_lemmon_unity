b9d6f1f748d69742d6f2750d5e1d477c653a9327
josegarcia19809 <josegarcia19809@gmail.com>
2024-08-02 08:16:05 -0600

    first commit

---
+++ GameEnding.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public GameObject player;
+    bool m_IsPlayerAtExit;
+    float m_Timer;
+    public float displayImageDuration = 1f;
+
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# john_lemmon_unity
